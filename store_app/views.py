be21ebe975eeafcf1126bf5f15240abb245472d5
AymanTareq.97 <tareq.ctec02@gmail.com>
2020-12-20 12:38:09 +0600

    configure basic urls, view and render templates

--- store_app/views.py
+++ store_app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def store(request):
+    context = {}
+    return render(request, 'store_app/store.html', context=context)
+
+def cart(request):
+    context = {}
+    return render(request, 'store_app/cart.html', context=context)
+
+def checkout(request):
+    context = {}
+    return render(request, 'store_app/checkout.html', context=context)
