bb522bc5c71457517d96c1a47807f7289ce2d4d3
AymanTareq.97 <tareq.ctec02@gmail.com>
2020-12-20 18:47:59 +0600

    creatd dome dummy product in store.html template

--- store_app/urls.py
+++ store_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.store, name='home'),
     path('cart/', views.cart, name='cart'),
+    path('store/', views.store, name='store'),
     path('checkout/', views.checkout, name='checkout'),
 ]
