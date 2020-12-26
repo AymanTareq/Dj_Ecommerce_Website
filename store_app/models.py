fec4a5ffc0868f5dde293b0ae7465cca2b83e504
AymanTareq <tareq.ctec02@gmail.com>
2020-12-26 16:14:32 +0600

    successfully, render data in cart and checkout templates

--- store_app/models.py
+++ store_app/models.py
@@ -18,6 +18,7 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+        
     @property
     def image_url(self):
         try:
@@ -32,6 +33,18 @@ class Order(models.Model):
     status = models.BooleanField(default=False)
     transaction_id = models.CharField(max_length=100,null=True)
 
+    @property
+    def get_cart_total(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.get_total for item in orderitems])
+        return total
+    
+    @property
+    def get_cart_items(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.quantity for item in orderitems])
+        return total
+
     def __str__(self):
         return str(self.id)
 
@@ -42,7 +55,12 @@ class OrderItem(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.product.name + '\t' + str(self.order.id)
+        return self.product.name + '\t order_id:' + str(self.order.id)
+
+    @property
+    def get_total(self):
+        total = self.product.price * self.quantity
+        return total
 
 class ShippingAddress(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
